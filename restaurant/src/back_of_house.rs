c5ade70a1e1808a5b9a8cf7ce27ebd49e797d670
YanceyOfficial <yanceyofficial@gmail.com>
2021-12-13 21:01:50 +0800

    feat: wip: learn use

---
+++ restaurant/src/back_of_house.rs
@@ -0,0 +1,9 @@
+pub mod cooking {
+    pub fn 蒸() {}
+
+    pub fn 煮() {}
+
+    pub fn 烤() {}
+
+    pub fn 炸() {}
+}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -7,12 +7,37 @@
 /// 此外, 它还有一个 src 文件夹, 里面有若干个 .rs 文件, 这些文件称为二进制 crate
 /// src 有两个强制命名的文件, main.rs 和 lib.rs, 如果是业务代码, 可以用 main.rs 作为整个项目的根
 /// 如果你在造轮子, 就用 lib.rs 作为整个库的根
+// 引用 front_of_house 模块
 mod front_of_house;
 
+mod back_of_house;
+
+mod use_struct;
+
+// 指定使用 front_of_house 模块中的 hosting 子模块
 pub use crate::front_of_house::hosting;
 
+pub use crate::back_of_house::cooking;
+
+pub use crate::use_struct::back_of_the_house::Breakfast;
+
+pub use crate::use_struct::back_of_the_house::Appetizer;
+
 pub fn eat_at_restaurant() {
+    // 通过绝对路径引用模块(rust 推荐使用绝对路径来引用)
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // 通过相对路径引用模块
+    front_of_house::hosting::add_to_waitlist();
+
+    // 通过 use 来引用
     hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
+    cooking::炸();
+
+    let mut instance = Breakfast::summer("Rye");
+    instance.toast = String::from("fuck");
+
+    // 只要给枚举加上 pub, 它所有的成员都是 pub 的
+    let a = Appetizer::Salad;
+    let b = Appetizer::Soup;
 }
