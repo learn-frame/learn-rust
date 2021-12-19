f589e3b9aa739860fe6b1428c10294c56effc002
YanceyOfficial <yanceyofficial@gmail.com>
2021-12-19 13:23:59 +0800

    feat: migrate

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,7 +1,6 @@
 // Rust 是静态类型(statically typed)语言, 也就是说在编译时就必须知道所有变量的类型
 use std::num::Wrapping;
 
-mod string;
 
 fn main() {
     // 当多种类型均有可能时, 必须增加类型注解
@@ -121,9 +120,4 @@ fn main() {
         println!("val is :{}", val);
     }
 
-    string::learn_string();
-    string::update_string();
-    string::index_string();
-    string::slice_string();
-    string::traverse_string();
 }
