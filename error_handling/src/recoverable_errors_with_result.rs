4253681f8778bbdd53707dd4bbc16deaaf23dbf4
YanceyOfficial <yanceyofficial@gmail.com>
2021-12-22 22:23:58 +0800

    feat: finish recoverable_errors_with_result

--- error_handling/src/recoverable_errors_with_result.rs
+++ error_handling/src/recoverable_errors_with_result.rs
@@ -1,3 +1,125 @@
-pub fn entry() {
+use std::error::Error;
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::ErrorKind;
 
-} 
+// 很多方法实现了 Result
+// 即结果成功了就走 OK, 失败了走 Err
+// 跟 try...catch 差不多
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
+pub fn entry() {
+    use_match_to_handling_file_result();
+    handle_specific_error();
+    read_file_by_closure();
+    panic_simple();
+
+    let p = propagating_errors();
+    match p {
+        Ok(_) => {}
+        Err(_) => {}
+    }
+}
+
+pub fn use_match_to_handling_file_result() {
+    let f = File::open("hello.txt");
+
+    match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem opening the file: {:?}", error)
+        }
+    };
+}
+
+pub fn handle_specific_error() {
+    let r_f = File::open("hello.txt");
+
+    match r_f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            // 匹配指定错误类型
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+}
+
+// 这是一种闭包的实现方式, 更优雅, TODO: 第十三章会讲到
+pub fn read_file_by_closure() {
+    File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+pub fn panic_simple() {
+    let f = File::open("hello.txt");
+
+    // unwrap 和 expect 都是返回文件句柄(成功时)或调用 panic! 宏(失败时)
+    // 不同的是, unwrap 失败给的是默认 error message, expect 可以自己传一个失败时的文案
+    #[allow(unused)]
+    let success = f.as_ref().unwrap();
+    #[allow(unused)]
+    let error = f.as_ref().expect("Failed to open hello.txt");
+}
+
+// -----------------------
+// 下面三种本质是一样的, 但越来越简单, 仔细体会
+
+// 使用纯粹的 match 样板代码
+#[allow(unused)]
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// 使用 ? 运算符, 更加优雅
+pub fn propagating_errors() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+// 跟上面一样, 这个更简单
+#[allow(unused)]
+pub fn simple_propagating_errors() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+// main 函数是特殊的, 其必须返回什么类型是有限制的
+// Box<dyn Error> 被称为 trait 对象(rait objec)
+// TODO: 十七章会解释
+// 目前可以理解 Box<dyn Error> 为使用 ? 时 main 允许返回的任何类型的错误
+#[allow(unused)]
+fn main() -> Result<(), Box<dyn Error>> {
+    File::open("hello.txt")?;
+
+    Ok(())
+}
--- error_handling/src/unrecoverable_errors_with_panic.rs
+++ error_handling/src/unrecoverable_errors_with_panic.rs
@@ -3,10 +3,10 @@
 /// 可恢复错误通常代表向用户报告错误和重试操作是合理的情况, 比如未找到文件
 /// 不可恢复错误其实就是 bug, 比如尝试访问超过数组结尾的位置
 ///
-/// 大大部分语言并不区分这两类错误, 并采用类似异常这样方式统一处理他们.
+/// 大部分语言并不区分这两类错误, 并采用类似异常这样方式统一处理他们.
 /// Rust 并没有异常, 但是, 有可恢复错误 Result<T, E> , 和不可恢复(遇到错误时停止程序执行)错误 panic!
 
-pub fn unrecoverable_errors_with_panic() {
+pub fn entry() {
     // thread 'main' panicked at 'crash and burn', src/main.rs:12:5
     // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
     // panic!("crash and burn");
