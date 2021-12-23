67ef68598348fc756eab8ba1982a664e53c8ee39
Yancey Leo <yanceyofficial@gmail.com>
2021-12-23 22:31:17 +0800

    feat: learn generics

--- generics/src/generics.rs
+++ generics/src/generics.rs
@@ -1,6 +1,8 @@
 pub fn entry() {
     // let list = vec![3, 0, 6, 1, 4, 5];
     // println!("The largest element is {}.", get_largest(&list));
+
+    multi_generics();
 }
 
 // pub fn get_largest<T>(list: &[T]) -> T {
@@ -15,17 +17,20 @@ pub fn entry() {
 //     largest
 // }
 
+#[allow(unused)]
 // 结构体可以定义泛型
 pub struct Point<T> {
     x: T,
     y: T,
 }
 
+#[allow(unused)]
 pub fn struct_generics() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
 }
 
+#[allow(unused)]
 impl<T> Point<T> {
     pub fn new(&self) -> Point<&T> {
         Point {
@@ -39,6 +44,7 @@ impl<T> Point<T> {
     }
 }
 
+#[allow(unused)]
 // 也可以给 Point 一个固定的泛型
 impl Point<f32> {
     fn distance_from_origin(&self) -> f32 {
@@ -46,22 +52,41 @@ impl Point<f32> {
     }
 }
 
-struct Point_1<T, U> {
+#[allow(unused)]
+// 也可以是多种泛型
+struct Point1<T, U> {
     x: T,
     y: U,
 }
 
-impl<T, U> Point_1<T, U> {
-    fn mixup<V, W>(self, other: Point_1<V, W>) -> Point_1<T, W> {
-        Point_1 {
+impl<T, U> Point1<T, U> {
+    fn mixup<V, W>(self, other: Point1<V, W>) -> Point1<T, W> {
+        Point1 {
             x: self.x,
             y: other.y,
         }
     }
 }
 
+fn multi_generics() {
+    let p1 = Point1 { x: 5, y: 10.4 };
+    let p2 = Point1 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y); // p3.x = 5, p3.y = c
+}
+
+#[allow(unused)]
 // 枚举也可以定义泛型
 enum Results<T, E> {
     Ok(T),
     Err(E),
 }
+
+// 泛型代码的性能
+// Rust 通过在编译时进行泛型代码的单态化(monomorphization)来保证效率.
+// 单态化是一个通过填充编译时使用的具体类型, 将通用代码转换为特定代码的过程.
+
+// let integer = Some(5);
+// let integer = Option_i32::Some(5);
