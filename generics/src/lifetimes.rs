7a475ac411336a18a8ed66c01526065f00e18214
YanceyOfficial <yanceyofficial@gmail.com>
2021-12-31 13:27:41 +0800

    feat: learn generics

--- generics/src/lifetimes.rs
+++ generics/src/lifetimes.rs
@@ -1,5 +1,9 @@
+use std::fmt::Display;
+
 pub fn entry() {
     bar();
+    println!("{}", first_word("Yancey Leo"));
+    println!("{}", longest_with_an_announcement("hi", "world", 1));
 }
 
 // 如下这个写法是错误的, 因为返回值是 s 的引用
@@ -88,12 +92,14 @@ pub fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 // 结构体的生命周期
 // 上回书说道, 我们在结构体中定义字符串的时候, 只用了 String, 而没有使用 &str
-
+// 如果你想用 string slice, 就必须得使用 'a
+#[allow(unused)]
 pub struct ImportantExcerpt<'a, T> {
     part: &'a str,
     age: T,
 }
 
+#[allow(unused)]
 pub fn foo() {
     let novel = String::from("Call me Ishmael. Some years ago...");
     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
@@ -102,4 +108,73 @@ pub fn foo() {
         age: 18,
     };
     println!("{}", i.part);
+}
+
+// 现在我们已经知道了每一个引用都有一个生命周期,
+// 而且我们需要为那些使用了引用的函数或结构体指定生命周期
+// 但下面这个例子定义了一个没有使用生命周期注解的函数, 即便其参数和返回值都是引用
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+// 这是因为上面这个例子就涉及到了生命周期省略(Lifetime Elision)
+// 被编码进 Rust 引用分析的模式被称为生命周期省略规则(lifetime elision rules),
+// 函数或方法的参数的生命周期被称为 输入生命周期(input lifetimes), 而返回值的生命周期被称为输出生命周期(output lifetimes)
+// 编译器共有三个规则:
+// 1. 每一个是引用的参数都有它自己的生命周期参数
+// 2. 如果只有一个输入生命周期参数, 那么它被赋予所有输出生命周期参数
+// 3. 如果方法有多个输入生命周期参数并且其中一个参数是 &self 或 &mut self, 说明是个对象的方法, 那么所有输出生命周期参数被赋予 self 的生命周期
+
+// 以 fn first_word(s: &str) -> &str {} 为例
+// 应用第一条规则, 变成了:
+// fn first_word<'a>(s: &'a str) -> &str {}
+// 再应用第二条规则, 变成了:
+// fn first_word<'a>(s: &'a str) -> &'a str {}
+
+// 再看另一个例子: fn longest(x: &str, y: &str) -> &str {}
+// 应用第一条规则, 变成了:
+// fn longest<'a, 'b>(x: &'a str, y: &'b str) -> &str {}
+// 然后它既不适合第二条规则, 也不适合第三条规则, 编译器就无法明确生命周期, 你就必须自己声明泛型注解了
+
+// 如上所述, 因为 ImportantExcerpt 上有个 string slice
+// 所以你必然需要加上生命周期注解了
+impl<'a, T> ImportantExcerpt<'a, T> {
+    #[allow(unused)]
+    fn level(&self) -> i32 {
+        3
+    }
+
+    #[allow(unused)]
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+#[allow(unused)]
+fn bar1() {
+    // 'static 的生命周期能够存活于整个程序期间. 所有的字符串字面值都拥有 'static 生命周期
+    #[allow(unused)]
+    let s: &'static str = "I have a static lifetime.";
+}
+
+// ann 的类型是泛型 T, 它可以被放入任何实现了 where 从句中指定的 Display trait 的类型
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, announcement: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", announcement);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
