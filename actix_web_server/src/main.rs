197a825547df94a63f44529b8e80104502c3c52b
YanceyOfficial <yanceyofficial@gmail.com>
2022-09-28 22:01:51 +0800

    feat: update

--- actix_web_server/src/main.rs
+++ actix_web_server/src/main.rs
@@ -3,8 +3,15 @@ use std::time::Duration;
 
 use actix_cors::Cors;
 use actix_web::{http::header, middleware, web, App, HttpResponse, HttpServer};
+use fake::{
+    faker::internet::en::{SafeEmail, Username},
+    faker::lorem::en::Paragraph,
+    faker::name::en::Name,
+    uuid::UUIDv4,
+    Fake,
+};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct User {
@@ -18,22 +25,44 @@ struct User {
     follow_count: i32,
 }
 
+fn generate_user() -> User {
+    User {
+        user_id: UUIDv4.fake(),
+        username: Username().fake(),
+        full_name: Name().fake(),
+        biography: Paragraph(3..5).fake(),
+        email: SafeEmail().fake(),
+        profile_pic_url: format!(
+            "https://dummyimage.com/{}x{}.png",
+            rand::thread_rng().gen_range(300..600),
+            rand::thread_rng().gen_range(300..600),
+        ),
+        follower_count: rand::thread_rng().gen_range(100..6000),
+        follow_count: rand::thread_rng().gen_range(100..6000),
+    }
+}
+
 /// This handler uses json extractor
 async fn get_user() -> HttpResponse {
     thread::sleep(Duration::from_millis(1000));
 
-    let user = User {
-        user_id:  Uuid::new_v4().to_string(),
-        username: String::from("YanceyOfficial"),
-        full_name: String::from("Yancey Leo"),
-        biography: String::from("Music Producer | Software Engineer"),
-        email: String::from("yanceyofficial@gmail.com"),
-        profile_pic_url: String::from("https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-19/50701353_260157948214698_4752524794996457472_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=5GQwsIVvTR0AX8fXQAi&tn=HlJjMFEZ3tLz5Hrx&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT9_Ev9_ls9gHQYTZsX-rFgnMk14wdIu06NIp2mO-gnb7A&oe=632FC93E&_nc_sid=8fd12b"),
-        follower_count: 1000,
-        follow_count: 100
-    };
+    let user = generate_user();
 
     let res = web::Json(user);
+
+    HttpResponse::Ok().json(res)
+}
+
+async fn get_users() -> HttpResponse {
+    thread::sleep(Duration::from_millis(1000));
+    let rand_num = rand::thread_rng().gen_range(2..10);
+    let mut users: Vec<User> = vec![];
+
+    for _ in (1..=rand_num).rev() {
+        users.push(generate_user());
+    }
+
+    let res = web::Json(users);
 
     HttpResponse::Ok().json(res)
 }
@@ -48,7 +77,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(
                 Cors::default()
-                    .allowed_origin("http://localhost:3000")
+                    .allowed_origin("http://127.0.0.1:3000")
                     .allowed_methods(vec!["GET", "POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
@@ -58,6 +87,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .app_data(web::JsonConfig::default().limit(4096))
             .service(web::resource("/user").route(web::get().to(get_user)))
+            .service(web::resource("/users").route(web::get().to(get_users)))
     })
     .bind(("127.0.0.1", 3002))?
     .run()
